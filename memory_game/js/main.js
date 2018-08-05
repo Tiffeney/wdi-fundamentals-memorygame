7169eb7c858a904cc26a14fb33624ded8d1fb472
Tiffeney Rambo <tea.r@Tiffeneys-MacBook-Pro-2.local>
2018-08-05 16:41:25 -0700

    created game logic (missing step)

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,13 @@
 var cards = ["queen" , "queen" , "king" , "king"];
 var cardsInPlay = [];
 
-var cardOne = [0];
-cardsInPlay.push("cardOne");
-console.log("User flipped queen");
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
 
-var cardTwo = [2];
-cardsInPlay.push("cardTwo");
-console.log("User flipped king");
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
 
 
 if (cardsInPlay.length ===2) {
