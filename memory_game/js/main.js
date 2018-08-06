68893d2562dac78a97b5b56a446f6455929baf32
Tiffeney Rambo <tea.r@Tiffeneys-MacBook-Pro-2.local>
2018-08-05 19:11:14 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,22 @@
 var cards = ["queen" , "queen" , "king" , "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-
-if (cardsInPlay.length ===2) {
+var checkForMatch = function ()	{
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+	console.log("You found a match!");
 	} else {
-	alert("Sorry, try again");
-}
+	console.log("Sorry, try again.");
+	}
 };
+
+var flipCard = function (cardId) { //create function called flipCard, () parameter cardId
+	console.log("User flipped " + cards[cardId]); //add a console.log statement to display the card was flipped
+		cardsInPlay.push(cards[cardId]);
+		
+	if (cardsInPlay.length === 2) {
+	checkForMatch(); 
+	}
+};
+
+flipCard (0) ; //Call the flipCard function passing in 0 as an argument
+flipCard (2) ; //call the flipCard function again, passing in 2 as an argument
