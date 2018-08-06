9f4a79d1dd8eb00656471724d20ca89f14247f58
Tiffeney Rambo <tea.r@Tiffeneys-MacBook-Pro-2.local>
2018-08-06 00:45:38 -0700

    created game logic - alert

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,18 +22,20 @@ var cards = [ // setup object
 ];
 
 var cardsInPlay = [];
-
-var checkForMatch = function ()	{
-		if(cardsInPlay[0] ===  cardsInPlay[1]) {
-			alert("You found a match!");
-		} else {
-			alert("Sorry, try again.");
+	var checkForMatch = function ()	{
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+	} else {
+	alert("Sorry, try again.");
 	}
 };
+
+
 
 var flipCard = function () { //create function called flipCard, () parameter cardId -- removed
 	var cardId = this.getAttribute('data-id');
 		console.log("User flipped " + (cards[cardId].rank) ); // Pay attebtion to bracket placement. EXTREMELY CRITICAL
+		cardsInPlay.push(cards[cardId].rank);
 		console.log(cards[cardId].cardImage); //add first statement
 		console.log(cards[cardId].suit); //add second statement
 		this.setAttribute('src', cards[cardId].cardImage)
@@ -41,6 +43,7 @@ var flipCard = function () { //create function called flipCard, () parameter car
 		checkForMatch();
 	}
 };
+
 		//action cleared console.log
 var createBoard = function () { //created function
 	for (var i = 0; i < cards.length; i++) { 
